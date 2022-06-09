12cdd487de7c89052dc14c2003913b80e97679b9
Norah Taylor <emt368@gmail.com>
2022-06-09 16:39:08 -0400

    commit tree c86bd8bea8ea0121fe32febec2c98305b56cb930

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,12 @@
 require 'pry'
+require_relative './memorable'
 
-class Artist
+class Artist 
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable
+  
   @@artists = []
 
   def initialize
@@ -19,13 +22,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
